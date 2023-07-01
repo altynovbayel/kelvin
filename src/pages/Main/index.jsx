4297ca4f1f962a02865c8f24c62b2e5d9f4229ad
altynovbayel <altynovich30@gmail.com>
2023-07-01 21:25:27 +0600

    update

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,6 +4,7 @@ import Banner from '../../components/Banner'
 import AboutBlock from '../../components/AboutBlock'
 import Brand from '../../components/Brand'
 import Products from '../../components/Products'
+import Facts from '../../components/Facts'
 
 const Main = () => {
   return (
@@ -12,6 +13,7 @@ const Main = () => {
       <AboutBlock/>
       <Brand/>
       <Products/>
+      <Facts/>
     </div>
   )
 }
