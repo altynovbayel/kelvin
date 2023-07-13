24e5862477a207f9ef009db6fdd9b1ef5cb76f34
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-07-13 19:05:39 +0600

    banner fixed

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,8 +5,14 @@ import { useViewportScroll, motion, useTransform } from 'framer-motion';
 
 
 const Banner = () => {
+  const [ scrollPix, setScrollPix ] = React.useState(0)
   const { scrollYProgress } = useViewportScroll()
   const scale = useTransform(scrollYProgress, [0, 1], [1, 4]);
+
+  window.onscroll =  () => {
+    setScrollPix(window.scrollY); // Value of scroll Y in px
+  };
+
 
   return (
     <motion.div className={c.banner}>
@@ -21,12 +27,21 @@ const Banner = () => {
         <div className={c.banner_subtitle}>
           <p>Делаем качественно, красиво и с душой</p>
         </div>
-        <motion.img 
-          src={banner} 
-          style={{
-            scale 
+        <div
+          style={scrollPix >= 1200 ?
+          {
+            overflow: 'hidden'
+          } : {
+            
           }}
-        />
+        >
+          <motion.img 
+            src={banner} 
+            style={{
+              scale 
+            }}
+          />
+        </div>
       </div>
     </motion.div>  
   )
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -12,22 +12,24 @@ const Main = () => {
     setScrollPixel(window.scrollY); // Value of scroll Y in px
   };
   return (
-    <div className={c.main}>
-      <Components.Banner/>
-      <motion.div
-        style={{
-          position: 'relative',
-          top: `-${scrollPixel+100}px`
-        }} 
-        className={c.scroll}
-      >
-        <Components.AboutBlock/>
-        {/* <LottieAnimation /> */}
-        <Components.Brand/>
-        <Components.Products/>
-        <Components.Facts/>
-      </motion.div>
-      
+    <div className={c.container}>
+      <div className={c.main}>
+        <Components.Banner/>
+        <motion.div
+          style={{
+            position: 'relative',
+            top: `-${scrollPixel+400}px`
+          }} 
+          className={c.scroll}
+        >
+          <Components.AboutBlock/>
+          {/* <LottieAnimation /> */}
+          <Components.Brand/>
+          <Components.Products/>
+          <Components.Facts/>
+        </motion.div>
+        
+      </div>
     </div>
   )
 }
