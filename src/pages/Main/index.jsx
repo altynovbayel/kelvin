1beadaef7c517169cca994e062db836e84d02856
altynovbayel <altynovich30@gmail.com>
2023-07-15 01:10:53 +0600

    cities

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -27,6 +27,7 @@ const Main = () => {
           <Components.Brand/>
           <Components.Products/>
           <Components.Facts/>
+          <Components.Logo/>
         </motion.div>
         
       </div>
